91ac4e7adf6c08dbeb4967c0d875e51da70be714
Nguyễn Dũng <103941900+KTY01it@users.noreply.github.com>
2023-04-10 10:26:52 +0700

    Add files via upload
    CODE xịn hơn

--- TakePhoto/TakePhoto.ino
+++ TakePhoto/TakePhoto.ino
@@ -1,9 +1,6 @@
 /*********
   Rui Santos
-  Complete instructions at https://RandomNerdTutorials.com/esp32-cam-projects-ebook/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files.
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  Complete project details at https://RandomNerdTutorials.com/esp32-cam-ov2640-camera-settings/
 *********/
 
 #include "esp_camera.h"
@@ -35,7 +32,7 @@
 // Keep track of number of pictures
 unsigned int pictureNumber = 0;
 
-// Stores the camera configuration parameters
+//Stores the camera configuration parameters
 camera_config_t config;
 
 void setup() {
@@ -43,25 +40,25 @@ void setup() {
  
   Serial.begin(115200);
   
-  // Initialize the camera  
+  //Initialize the camera  
   Serial.print("Initializing the camera module...");
   configInitCamera();
   Serial.println("Ok!");
  
-  // Initialize MicroSD
+  //Initialize MicroSD
   Serial.print("Initializing the MicroSD card module... ");
   initMicroSDCard();
 }
 
 void loop() {
-  // Path where new picture will be saved in SD Card
+  //Path where new picture will be saved in SD Card
   String path = "/picture" + String(pictureNumber) +".jpg";  
   Serial.printf("Picture file name: %s\n", path.c_str());
 
-  // Take and Save Photo
+  //Take and Save Photo
   takeSavePhoto(path);
   pictureNumber++;
-  delay(5000); 
+  delay(2000); 
 }
 
 void configInitCamera(){
@@ -89,7 +86,7 @@ void configInitCamera(){
   // Select lower framesize if the camera doesn't support PSRAM
   if(psramFound()){
     config.frame_size = FRAMESIZE_UXGA; // FRAMESIZE_ + QVGA|CIF|VGA|SVGA|XGA|SXGA|UXGA
-    config.jpeg_quality = 10; //0-63 lower number means higher quality
+    config.jpeg_quality = 10; //10-63 lower number means higher quality
     config.fb_count = 2;
   } else {
     config.frame_size = FRAMESIZE_SVGA;
@@ -103,6 +100,30 @@ void configInitCamera(){
     Serial.printf("Camera init failed with error 0x%x", err);
     return;
   }
+
+  sensor_t * s = esp_camera_sensor_get();
+  s->set_brightness(s, 0);     // -2 to 2
+  s->set_contrast(s, 0);       // -2 to 2
+  s->set_saturation(s, 0);     // -2 to 2
+  s->set_special_effect(s, 2); // 0 to 6 (0 - No Effect, 1 - Negative, 2 - Grayscale, 3 - Red Tint, 4 - Green Tint, 5 - Blue Tint, 6 - Sepia)
+  s->set_whitebal(s, 1);       // 0 = disable , 1 = enable
+  s->set_awb_gain(s, 1);       // 0 = disable , 1 = enable
+  s->set_wb_mode(s, 0);        // 0 to 4 - if awb_gain enabled (0 - Auto, 1 - Sunny, 2 - Cloudy, 3 - Office, 4 - Home)
+  s->set_exposure_ctrl(s, 1);  // 0 = disable , 1 = enable
+  s->set_aec2(s, 0);           // 0 = disable , 1 = enable
+  s->set_ae_level(s, 0);       // -2 to 2
+  s->set_aec_value(s, 300);    // 0 to 1200
+  s->set_gain_ctrl(s, 1);      // 0 = disable , 1 = enable
+  s->set_agc_gain(s, 0);       // 0 to 30
+  s->set_gainceiling(s, (gainceiling_t)0);  // 0 to 6
+  s->set_bpc(s, 0);            // 0 = disable , 1 = enable
+  s->set_wpc(s, 1);            // 0 = disable , 1 = enable
+  s->set_raw_gma(s, 1);        // 0 = disable , 1 = enable
+  s->set_lenc(s, 1);           // 0 = disable , 1 = enable
+  s->set_hmirror(s, 0);        // 0 = disable , 1 = enable
+  s->set_vflip(s, 0);          // 0 = disable , 1 = enable
+  s->set_dcw(s, 1);            // 0 = disable , 1 = enable
+  s->set_colorbar(s, 0);       // 0 = disable , 1 = enable
 }
 
 void initMicroSDCard(){
@@ -121,7 +142,10 @@ void initMicroSDCard(){
 
 void takeSavePhoto(String path){
   // Take Picture with Camera
-  camera_fb_t  * fb = esp_camera_fb_get();  
+  camera_fb_t  * fb = esp_camera_fb_get();
+
+  
+  
   if(!fb) {
     Serial.println("Camera capture failed");
     return;
